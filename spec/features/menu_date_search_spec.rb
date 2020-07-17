d7bba46df3a69f426577c6292231700ad8167986
AnatolyHohuda <hohudatolia@gmail.com>
2020-07-17 08:41:30 +0300

    Orders search by date spec created

--- spec/features/menu_date_search_spec.rb
+++ spec/features/menu_date_search_spec.rb
@@ -4,39 +4,28 @@ RSpec.feature "MenuDateSearches", type: :feature do
   let!(:admin) { create(:admin) }
   let!(:user) { create(:user) }
   let!(:today_menu) { create(:menu, created_at: Date.today) }
-  let!(:yesterday_menu) { create(:menu, created_at: Date.yesterday) }
   let!(:week_ago_menu) { create(:menu, created_at: Date.today - 7.days) }
 
   before do
     sign_in(user)
   end
 
-  context 'when searching by date' do
-    it 'should search for today menus' do
-      visit user_path(user)
+  it 'should search for today menus' do
+    visit user_path(user)
 
-      fill_in "search[menu_date]", with: Date.today.to_s
-      click_button('Search for menus')
-      expect(page).to have_content(today_menu.name)
-      expect(page).not_to have_content(yesterday_menu.name)
-    end
+    fill_in "search[menu_date]", with: Date.today.to_s
+    click_button('Search for menus')
+    expect(page).to have_content(today_menu.name)
+    expect(page).not_to have_content(week_ago_menu.name)
+  end
 
-    it 'should search for yesterday menus' do
-      visit user_path(user)
 
-      fill_in "search[menu_date]", with: Date.yesterday.to_s
-      click_button('Search for menus')
-      expect(page).to have_content(yesterday_menu.name)
-      expect(page).not_to have_content(week_ago_menu.name)
-    end
+  it 'should search for week ago menu' do
+    visit user_path(user)
 
-    it 'should search for week ago menu' do
-      visit user_path(user)
-
-      fill_in "search[menu_date]", with: (Date.today - 7.days).to_s
-      click_button('Search for menus')
-      expect(page).to have_content(week_ago_menu.name)
-      expect(page).not_to have_content(today_menu.name)
-    end
+    fill_in "search[menu_date]", with: (Date.today - 7.days).to_s
+    click_button('Search for menus')
+    expect(page).to have_content(week_ago_menu.name)
+    expect(page).not_to have_content(today_menu.name)
   end
 end
--- spec/features/order_date_search_spec.rb
+++ spec/features/order_date_search_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.feature "OrderDateSearches", type: :feature do
-  
+  let!(:admin) { create(:admin) }
+  let!(:user) { create(:user) }
+  let(:menu) { create(:menu) }
+  let!(:today_order) { create(:order, user: user, menu: menu) }
+  let!(:week_ago_order) { create(:order, user: user, menu: menu, created_at: (Date.today - 7.days)) }
+
+  before(:each) do
+    sign_in(admin)
+  end
+
+  it 'should search for today orders' do
+    visit orders_path
+
+    fill_in 'search[order_date]', with: Date.today.to_s
+    click_button('Search')
+
+    expect(page).to have_content("Order ##{today_order.id}")
+    expect(page).not_to have_content("Order ##{week_ago_order.id}")
+  end
+
+  it 'should search for week ago orders' do
+    visit orders_path
+
+    fill_in 'search[order_date]', with: (Date.today - 7.days).to_s
+    click_button('Search')
+
+    expect(page).to have_content("Order ##{week_ago_order.id}")
+    expect(page).not_to have_content("Order ##{today_order.id}")
+  end
 end
